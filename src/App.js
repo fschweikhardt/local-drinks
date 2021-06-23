d91edc496827267b79254036178f8b7bf1fa1f45
fschweikhardt <schweikhardt.web@gmail.com>
2021-06-23 17:06:37 -0400

    functional map

--- src/App.js
+++ src/App.js
@@ -1,30 +1,200 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react'
-import config from './config'
+// import React, { Component } from 'react';
+// import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react'
+// import config from './config'
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
+// // {
+// //   lat: 39.1636505,
+// //   lng: -86.525757
+// // }
+
+// const mapStyles = {
+//   width: '100%',
+//   height: '100%'
+// };
+
+// export class MapContainer extends Component {
+
+//   state = {
+//     showingInfoWindow: false,  // Hides or shows the InfoWindow
+//     activeMarker: {},          // Shows the active marker upon click
+//     selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+//   };
+
+//   onMarkerClick = (props, marker, e) =>
+//     this.setState({
+//       selectedPlace: props,
+//       activeMarker: marker,
+//       showingInfoWindow: true
+//   });
+
+//   onClose = props => {
+//     if (this.state.showingInfoWindow) {
+//       this.setState({
+//         showingInfoWindow: false,
+//         activeMarker: null
+//       });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <Map
+//         google={this.props.google}
+//         zoom={14}
+//         style={mapStyles}
+//         initialCenter={
+//           {lat: -1.2884,
+//             lng: 36.8233}
+//         }
+//         >
+
+//         <Marker
+//           onClick={this.onMarkerClick}
+//           name={'Kenyatta International Convention Centre'}
+//         />
+
+        
+//         <InfoWindow
+//             marker={this.state.activeMarker}
+//             visible={this.state.showingInfoWindow}
+//             onClose={this.onClose}
+//           >
+//           <div>
+//             <h4>{this.state.selectedPlace.name}</h4>
+//           </div>
+//         </InfoWindow>
+
+//       </Map>
+//     );
+//   }
+// }
+
+// export default GoogleApiWrapper({
+//   apiKey: config.MAPS_KEY
+// })(MapContainer);
+
+
+import React from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
+import config from './config';
+
+const containerStyle = {
+  width: '800px',
+  height: '800px'
 };
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={
-          {
-            lat: -1.2884,
-            lng: 36.8233
-          }
-        }
-      />
-    );
+const center = {
+    lat: 39.1636505,
+    lng: -86.525757
   }
+
+const places = [
+  { 
+    name: 'location one',
+    location: {
+      lat: 39.16,
+      lng: -86.52
+    }
+  },
+  { 
+    name: 'location two',
+    location: {
+      lat: 39.1,
+      lng: -86.5
+    }
+  },
+]
+
+export default function MyComponent() {
+  return (
+    <LoadScript
+      googleMapsApiKey={config.MAPS_KEY}
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+      >
+        { /* Child components, such as markers, info windows, etc. */ 
+          places.map( place => {
+            return (
+
+            <Marker key={place.name} position={place.location}/>
+          )})
+        }
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default GoogleApiWrapper({
-  apiKey: config.MAPS_KEY
-})(MapContainer);
+// import React from 'react';
+// import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+// import config from './config';
+
+// export default function MapContainer() {
+  
+//   const mapStyles = {        
+//     height: "100vh",
+//     width: "100%"};
+  
+//   const defaultCenter = {
+//     lat: 41.3851, lng: 2.1734
+//   }
+
+//   const locations = [
+//     {
+//       name: "LOCATION 1",
+//       location: { 
+//         lat: 41.3954,
+//         lng: 2.162 
+//       },
+//     },
+//     {
+//       name: "Location 2",
+//       location: { 
+//         lat: 41.3917,
+//         lng: 2.1649
+//       },
+//     },
+//     {
+//       name: "Location 3",
+//       location: { 
+//         lat: 41.3773,
+//         lng: 2.1585
+//       },
+//     },
+//     {
+//       name: "Location 4",
+//       location: { 
+//         lat: 41.3797,
+//         lng: 2.1682
+//       },
+//     },
+//     {
+//       name: "Location 5",
+//       location: { 
+//         lat: 41.4055,
+//         lng: 2.1915
+//       },
+//     }
+//   ];
+  
+//   return (
+//      <LoadScript
+//        googleMapsApiKey={config.MAPS_KEY}>
+//         <GoogleMap
+//           mapContainerStyle={mapStyles}
+//           zoom={13}
+//           center={defaultCenter}
+//             {
+//               ...locations ? locations.map(item => {
+//                 return (
+//                 <Marker key={item.name} position={item.location}/>
+//                 )
+//               }) : null
+//           }
+//         />
+//      </LoadScript>
+//   )
+
+// }
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 export default {
     MAPS_KEY: process.env.REACT_APP_MAPS_KEY
 }
